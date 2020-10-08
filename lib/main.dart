494df73a5fc858c6f816b6f53e4ef57a08185e5f
anubhav180400 <anubhavrajput1804@gmail.com>
2020-10-08 22:51:45 +0530

    added REGISTRATION screens

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,7 @@ import 'package:grocery_app/Screens/Registration/signup.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:grocery_app/Screens/Registration/home.dart';
-import 'package:grocery_app/contact.dart;
+import 'package:grocery_app/Screens/ContactUs/contact.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
