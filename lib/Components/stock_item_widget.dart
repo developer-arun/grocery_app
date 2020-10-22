817ed87b8f3cf2b6fbbaf4526e71134c331d1b5d
Harsh Gyanchandani <harsh.gyanchandani@gmail.com>
2020-10-23 00:10:40 +0530

    loaded current stock in store

--- lib/Components/stock_item_widget.dart
+++ lib/Components/stock_item_widget.dart
@@ -1,18 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:grocery_app/Model/Product.dart';
 import 'package:grocery_app/utilities/constants.dart';
 
 class StockItemWidget extends StatelessWidget {
-  const StockItemWidget({
-    Key key,
-  }) : super(key: key);
+
+  final Product product;
+  const StockItemWidget({@required this.product});
 
   @override
   Widget build(BuildContext context) {
     return Container(
       width: 150,
-      height: 180,
       padding: const EdgeInsets.all(5),
-      margin: const EdgeInsets.only(right: 20),
       decoration: BoxDecoration(
         color: kColorWhite,
         borderRadius: BorderRadius.all(Radius.circular(10)),
@@ -30,7 +29,10 @@ class StockItemWidget extends StatelessWidget {
           Expanded(
             child: Container(
               decoration: BoxDecoration(
-                // TODO: ADD DECORATION IMAGE
+                image: DecorationImage(
+                  image: NetworkImage(product.imageURL),
+                  fit: BoxFit.cover,
+                ),
                 borderRadius: BorderRadius.all(Radius.circular(10)),
                 color: kColorPurple,
               ),
@@ -42,7 +44,7 @@ class StockItemWidget extends StatelessWidget {
           Padding(
             padding: const EdgeInsets.symmetric(horizontal: 8,vertical: 0),
             child: Text(
-              'Apple',
+              product.name,
               style: TextStyle(
                 color: kColorPurple,
                 fontSize: 18,
@@ -53,50 +55,16 @@ class StockItemWidget extends StatelessWidget {
           SizedBox(
             height: 2,
           ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: <Widget>[
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 8,vertical: 0),
-                child: Container(
-                  padding: EdgeInsets.symmetric(horizontal: 5,vertical: 4),
-                  decoration: BoxDecoration(
-                    color: Colors.red[100],
-                    borderRadius: BorderRadius.circular(20),
-                    boxShadow: [BoxShadow(color: Colors.redAccent,blurRadius: 0.0,spreadRadius: 1),]
-                  ),
-                  child: Text(
-                    '2 Kg',
-                    style: TextStyle(
-                      color: Colors.red[900],
-                      fontSize: 12,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 8,vertical: 0),
+            child: Text(
+              '${product.quantity} Kgs',
+              style: TextStyle(
+                color: kColorPurple,
+                fontSize: 12,
+                fontWeight: FontWeight.bold,
               ),
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 8,vertical: 0),
-                child: Container(
-                  padding: EdgeInsets.symmetric(horizontal: 5,vertical: 4),
-                  decoration: BoxDecoration(
-                    color: Colors.green[200],
-                    borderRadius: BorderRadius.circular(20),
-                    boxShadow: [
-                      BoxShadow(color: Colors.green,blurRadius: 0.0,spreadRadius: 1),
-                    ]
-                  ),
-                  child: Text(
-                    '\$ 1.99',
-                    style: TextStyle(
-                      color: Colors.green[900],
-                      fontSize: 16,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                ),
-              ),
-            ],
+            ),
           ),
           SizedBox(
             height: 5,
