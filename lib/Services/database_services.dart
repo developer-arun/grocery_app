10d2e45f0f5e90de1cff7e302e32002f05de3779
anubhav180400 <anubhavrajput1804@gmail.com>
2020-10-24 00:11:51 +0530

    adding getProductByRating

--- lib/Services/database_services.dart
+++ lib/Services/database_services.dart
@@ -113,7 +113,7 @@ class DatabaseServices {
   in decreasing order of rating
    */
   static Future<List<Product>> getProductsByRating() async {
-
+ 
     List<Product> product = [];
     var firestoreInstance = FirebaseFirestore.instance;
     await firestoreInstance
@@ -148,4 +148,44 @@ class DatabaseServices {
     return product;
   }
 
+
+  /*
+  Function to fetch p details in user's city and country from database
+  in decreasing order of rating
+   */
+  static Future<List<Product>> getSellerByRating() async {
+
+    List<Product> product = [];
+    var firestoreInstance = FirebaseFirestore.instance;
+    await firestoreInstance
+        .collection("Products")
+        .where("city", isEqualTo: (UserApi.instance).getCity())
+        .where("country", isEqualTo: UserApi.instance.getCountry())
+        .orderBy("rating",descending: true)
+        .limit(5)
+        .get()
+        .then((result) {
+      for (var element in result.docs) {
+        product.add(Product(
+          id: element.data()["itemId"],
+          name: element.data()["name"],
+          desc: element.data()["description"],
+          ownerEmail: element.data()["storeId"],
+          price: element.data()["price"],
+          quantity: element.data()["quantity"],
+          rating: element.data()["rating"],
+          reviews: element.data()["reviews"],
+          orders: element.data()["orders"],
+          imageURL: element.data()["imageurl"],
+          category: element.data()["category"],
+          timestamp: int.parse(element.data()["timestamp"]),
+          city: UserApi.instance.getCity(),
+          country: UserApi.instance.getCountry(),
+        ));
+      }
+    }).catchError((error) {
+      print(error);
+    });
+    return product;
+  }
 }
