9a3f17a2be769b56115268df2a5366762df50252
Harsh Gyanchandani <harsh.gyanchandani@gmail.com>
2020-10-23 18:58:54 +0530

    loaded top 5 products in home screen

--- lib/Components/productsPager.dart
+++ lib/Components/productsPager.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:grocery_app/Components/ListData.dart';
+import 'package:grocery_app/Model/Product.dart';
+import 'package:grocery_app/Services/database_services.dart';
 import 'package:grocery_app/utilities/constants.dart';
 
 class ProductsPager extends StatefulWidget {
@@ -9,18 +11,19 @@ class ProductsPager extends StatefulWidget {
 }
 
 class _ProductsPagerState extends State<ProductsPager> {
+  List<Product> products = [];
 
-  List<ListData> promotions = [
-    ListData(
-        url:
-        "https://images.pexels.com/photos/89778/strawberries-frisch-ripe-sweet-89778.jpeg?auto=compress&cs=tinysrgb&h=650&w=940"),
-    ListData(
-        url:
-        "https://images.pexels.com/photos/89778/strawberries-frisch-ripe-sweet-89778.jpeg?auto=compress&cs=tinysrgb&h=650&w=940"),
-    ListData(
-        url:
-        "https://images.pexels.com/photos/89778/strawberries-frisch-ripe-sweet-89778.jpeg?auto=compress&cs=tinysrgb&h=650&w=940"),
-  ];
+  void getProducts() async {
+    products = await DatabaseServices.getProductsByRating();
+    setState(() {});
+  }
+
+  @override
+  void initState() {
+    super.initState();
+
+    getProducts();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -28,24 +31,27 @@ class _ProductsPagerState extends State<ProductsPager> {
       height: 150,
       child: ListView.builder(
         physics: BouncingScrollPhysics(),
-        itemCount: 3,
+        itemCount: products.length,
         scrollDirection: Axis.horizontal,
-        itemBuilder: (BuildContext context,int index){
+        itemBuilder: (BuildContext context, int index) {
           return Container(
-            margin: const EdgeInsets.only(left: 10,right: 5,top: 10,bottom: 10),
+            margin:
+                const EdgeInsets.only(left: 10, right: 5, top: 10, bottom: 10),
             child: AspectRatio(
               aspectRatio: 1,
               child: Container(
                 height: 120,
+                padding: const EdgeInsets.all(5),
                 decoration: BoxDecoration(
                   color: kColorWhite,
                   shape: BoxShape.circle,
                   image: DecorationImage(
                     image: NetworkImage(
-                      promotions[index].url,
+                     products[index].imageURL,
                     ),
                     fit: BoxFit.cover,
-                    colorFilter: ColorFilter.mode(kColorPurple.withOpacity(0.5), BlendMode.srcATop),
+                    colorFilter: ColorFilter.mode(
+                        kColorPurple.withOpacity(0.5), BlendMode.srcATop),
                   ),
                   boxShadow: [
                     BoxShadow(
@@ -54,6 +60,17 @@ class _ProductsPagerState extends State<ProductsPager> {
                       blurRadius: 2,
                     ),
                   ],
+                ),
+                child: Center(
+                  child: Text(
+                    products[index].name,
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      color: kColorWhite,
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold
+                    ),
+                  ),
                 ),
               ),
             ),
